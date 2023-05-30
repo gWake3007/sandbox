6acab872e45a3e85fe2f4431cbccaa93eb87febd
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-05-30 10:04:39 +0300

    1

---
+++ js/homework.js
@@ -0,0 +1,17 @@
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message =
+//   "You ordered droids worth totalPrice credits. Delivery (+'deliveryFee' credits) is included in total price.";
+// console.log(message);
+
+// Change code below this line
+function add(a, b, c) {
+  console.log("Addition result equals ${a+b+c}");
+  // Change code above this line
+}
+
+add(15, 27, 10);
+add(10, 20, 30);
+add(5, 10, 15);
