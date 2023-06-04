33953988872e2fa2d0a3c1716988ba1268673cfe
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-06-04 23:51:36 +0300

    1

---
+++ js/practical.js
@@ -0,0 +1,55 @@
+// Є деякі рядки, які потрібно перевірити на вміст заборонених слів spam і sale, і повернути результат перевірки. Слова в рядку можуть бути у довільному регістрі, наприклад SPAM або sAlE. Якщо знайшли заборонене слово (spam або sale) то повертайте true. Якщо в рядку відсутні заборонені слова, повертайте false.
+// Попорядку перевіряйте наступні рядки:
+
+// "Latest technology news" - false
+// "JavaScript weekly newsletter" - false
+// "Get best sale offers now!" - true
+// "Amazing SalE, only tonight!" - true
+// "Trust me, this is not a spam message" - true
+// "Get rid of sPaM emails. Our book in on sale!" - true
+
+// function checkString(str){
+    
+//     let isSpam;
+//     str = str.toLowerCase();
+    
+//     if(str.includes("spam") || str.includes("sale")){
+//         isSpam = true
+//     }
+//     else{
+//         isSpam = false
+//     }
+//     console.log(isSpam);
+// }
+// checkString("Latest technology news")
+// checkString("Amazing SalE, only tonight!")
+
+// function checkString(str){
+    
+//     let isSpam = false;
+//     str = str.toLowerCase();
+    
+//     if(str.includes("spam") || str.includes("sale")){
+//         isSpam = true
+//     }
+//     console.log(isSpam);
+// }
+// checkString("Latest technology news")
+// checkString("Amazing SalE, only tonight!")
+
+
+function checkString(str){
+    str = str.toLowerCase();
+    let isSpam = str.includes("spam") || str.includes("sale");
+    console.log(isSpam);
+}
+checkString("Latest technology news")
+checkString("Amazing SalE, only tonight!")
+
+// подивитись і цей зназок
+
+// function checkString(str){
+//     const isSpam =
+//       str.toLowerCase().includes("spam") ||
+//       str.toLowerCase().includes("sale");
+//   }
