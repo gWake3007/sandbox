03e61582aa8d8a3c99edffa089e78f46d09d9c21
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-09-13 08:04:06 +0200

    123

--- js/practical.js
+++ js/practical.js
@@ -83,8 +83,6 @@
 // }
 // // Change code above this line
 
-
-
 // разница между методами
 
 // const books = [
@@ -107,7 +105,6 @@
 // // Change code below this line
 
 // const genres = books.flatMap(book => book.genres);
-
 
 // const books = [
 //   {
@@ -132,8 +129,6 @@
 
 // const titles = books.map(book => book.title);
 
-
-
 // const players = [
 //     { name: "Mango", playtime: 1270, gamesPlayed: 4 },
 //     { name: "Poly", playtime: 469, gamesPlayed: 2 },
@@ -141,7 +136,7 @@
 //     { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
 //   ];
 //   // Change code below this line
-  
+
 //   const totalAveragePlaytimePerGame = players.reduce((time, player) => {
 //     return time + player.playtime;
 //   }, 0);
@@ -154,7 +149,6 @@
 // // Змініть код лише над цим рядком
 // console.log(myStr);
 
-
 // const myNoun = "dog";
 // const myAdjective = "big";
 // const myVerb = "ran";
@@ -164,8 +158,6 @@
 // const wordBlanks = myAdjective + myNoun + myVerb + myAdverb; // Змініть цей рядок
 // // Змініть код лише над цим рядком
 // console.log(wordBlanks);
-
-
 
 // const varMas = 10
 // console.log(Math.random() * varMas);
@@ -190,12 +182,6 @@
 
 // console.log(repetFunction(geometricProgresion, 5));
 
-
-
-
-
-
-
 // function geometricProgression(a, r, n) {
 //     if (n === 1) {
 //       return a;
@@ -205,30 +191,148 @@
 //       return a * geometricProgression(a * r, r, n - 1);
 //     }
 //   }
-  
+
 //   // Пример использования функции
 //   const firstTerm = 2; // Первый член прогрессии
 //   const commonRatio = 2; // Знаменатель прогрессии
 //   const termNumber = 5; // Номер члена прогрессии, который нужно найти
-  
+
 //   const result = geometricProgression(firstTerm, commonRatio, termNumber);
 //   console.log(`Член геометрической прогрессии ${termNumber} равен ${result}`);
-
-
 
 // function nextInLine(arr, item) {
 //     // Змініть код лише під цим рядком
 //     arr.push(item);
 //     const removed = arr.shift();
 //     return removed;
-    
+
 //     // Змініть код лише над цим рядком
 //   }
-  
+
 //   // Налаштування
 //   let testArr = [1, 2, 3, 4, 5];
-  
+
 //   // Відобразити код
 //   console.log("Before: " + JSON.stringify(testArr));
 //   console.log(nextInLine(testArr, 6));
 //   console.log("After: " + JSON.stringify(testArr));
+
+// --------------------------------------Примеры функции case-------------------------------------------
+// function caseInSwitch(val) {
+//   let answer = "";
+//   // Змініть код лише під цим рядком
+//   switch (val) {
+//     case 1:
+//       answer = "alpha";
+//       break;
+//     case 2:
+//       answer = "beta";
+//       break;
+//     case 3:
+//       answer = "gamma";
+//       break;
+//     case 4:
+//       answer = "delta";
+//       break;
+//   }
+
+//   // Змініть код лише над цим рядком
+//   return answer;
+// }
+
+// caseInSwitch(1);
+
+// function switchOfStuff(val) {
+//   let answer = "";
+//   // Змініть код лише під цим рядком
+//   switch (val) {
+//     case "a":
+//       answer = "apple";
+//       break;
+//     case "b":
+//       answer = "bird";
+//       break;
+//     case "c":
+//       answer = "cat";
+//       break;
+//     default:
+//       answer = "stuff";
+//       break;
+//   }
+
+//   // Змініть код лише над цим рядком
+//   return answer;
+// }
+
+// switchOfStuff(1);
+
+// function sequentialSizes(val) {
+//   let answer = "";
+//   // Змініть код лише під цим рядком
+//   switch (val) {
+//     case 1:
+//     case 2:
+//     case 3:
+//       answer = "Low";
+//       break;
+//     case 4:
+//     case 5:
+//     case 6:
+//       answer = "Mid";
+//       break;
+//     case 7:
+//     case 8:
+//     case 9:
+//       answer = "High";
+//       break;
+//   }
+
+//   // Змініть код лише над цим рядком
+//   return answer;
+// }
+
+// sequentialSizes(1);
+
+// function chainToSwitch(val) {
+//   let answer = "";
+//   // Змініть код лише під цим рядком
+
+//   // if (val === "bob") {
+//   //   answer = "Marley";
+//   // } else if (val === 42) {
+//   //   answer = "The Answer";
+//   // } else if (val === 1) {
+//   //   answer = "There is no #1";
+//   // } else if (val === 99) {
+//   //   answer = "Missed me by this much!";
+//   // } else if (val === 7) {
+//   //   answer = "Ate Nine";
+//   // }
+
+//   switch (val) {
+//     case 1:
+//       answer = "There is no #1";
+//       break;
+//     case 7:
+//       answer = "Ate Nine";
+//       break;
+//     case 42:
+//       answer = "The Answer";
+//       break;
+//     case 99:
+//       answer = "Missed me by this much!";
+//       break;
+//     case "bob":
+//       answer = "Marley";
+//       break;
+//     default:
+//       answer = "";
+//       break;
+//   }
+
+//   // Змініть код лише над цим рядком
+//   return answer;
+// }
+
+// chainToSwitch(7);
+
