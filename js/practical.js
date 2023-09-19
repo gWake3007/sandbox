abba8c8d52df171c0f23bbd48b57849cf3821553
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-09-19 19:37:56 +0200

    123

--- js/practical.js
+++ js/practical.js
@@ -535,3 +535,40 @@
 //   const secondTree = myPlants[1].list[1];
 //   console.log(secondTree);
 
+// Налаштування
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  // Змініть код лише під цим рядком
+  function updateRecords(records, id, prop, value) {
+    if(value === ""){
+      return records.id.slice(prop);
+    }else if(prop != records.id.prop(tracks) && value != ""){
+      return records.id.prop + value;
+    }else if(prop === records.id["tracks"] && value != ""){
+      return records.id["tracks"] + value;
+    }else if(records.hasOwnProperty("tracks")){
+      return records.id["tracks"] + value;
+    }
+    
+    return records;
+  }
+  
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
