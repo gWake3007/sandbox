469e1c2e8a2811f9e4fcb3e9718576ddf16c3f1c
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-09-25 19:28:02 +0200

    123

--- js/Lesson_2_Branchen_and_Cycles.js
+++ js/Lesson_2_Branchen_and_Cycles.js
@@ -1,7 +1,7 @@
-//  Example (if , else branch and Ternary Operator (Тернальний оператор -коротка функція з поверненням двух результатів true або false))
-let message = "";
+// //  Example (if , else branch and Ternary Operator (Тернальний оператор -коротка функція з поверненням двух результатів true або false))
+// let message = "";
 
-let userStatus = true;
+// let userStatus = false;
 
 // if(userStatus){
 //     message = "Admin is user!";
@@ -16,6 +16,27 @@ let userStatus = true;
 //     message = "Admin is not user!";
 // }
 
-message = userStatus ? "Admin is user!" : "Admin is not user!";
+// message = userStatus ? "Admin is user!" : "Admin is not user!";
 
-console.log(message);
+// message = `Admin is ${userStatus ? "" : "not"} user!`;
+
+// console.log(message);
+
+// //  ================================= Example (Cycle for)===================================================
+// for(let i = 0; i <= 10; i++){                  //i++  спочатку повертає значення а потім збільшує
+//     console.log(i);
+// }
+
+// for(let i = 0; i <= 10; i += 1){
+//     console.log(i);
+// }
+
+// for(let i = 0; i <= 10; ++i){                 // ++i  спочатку збільшує а потім повертає значення
+//     console.log(i);
+// }
+
+for(let i = 1; i <= 10; i += 1){                     // Цикл в циклі
+    for(let k = 1; k <= 5; k +=1){
+        console.log(`i: ${i} | k ${k}`);
+    }
+}
