0732a3615ee1e72d96c5051c119fca4c78a4178e
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-10-31 08:01:22 +0100

    123

--- js/Study_for_Mentor_Lesson_1.js
+++ js/Study_for_Mentor_Lesson_1.js
@@ -1,4 +1,4 @@
-//!=============================================== First Task ======================================================
+//!=============================================== First Task (If else and prompt) ======================================================
 // let question = prompt("Введіть будь ласка ваш емейл");
 // console.log(question);
 
@@ -10,4 +10,61 @@
 //     }
 // }
 
-//!===================================== Second Task =========================================
+//!===================================== Second Task (String Method and if else) =========================================
+// const firstString = "Vestibulum facilisis purus nec";
+// const secondString = "Nunc sed turpis a felis in nunc fringilla";
+// console.log(firstString);
+// console.log(secondString);
+
+// const changeFirstString = firstString.slice(0, 25);
+// console.log(changeFirstString);
+// console.log(changeFirstString + "...");
+
+// const changeSecondString = secondString.slice(0, 25);    //Залишаємо 25 перших знаків.
+// console.log(changeSecondString);
+// console.log(changeSecondString + "...");       //До рядка додаємо в кінці три крапки.
+
+// if(firstString.length > 25) {
+//     console.log(firstString.slice(0, 25) + "..."); //Умова при якій якщо рядок довший за 25 символів то береться 25 символів і три крапки
+// } else {
+//     console.log(firstString);
+// }
+
+//!=================================== Third Task (Methods .toLowerCase and .includes ) ==================================
+// function checkString(string) {            //Звичайна функція.
+//   string = string.toLowerCase();
+//   if (string.includes("spam") || string.includes("sale")) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function checkStringShortCode(string) {     //Коротка функція.
+//     string = string.toLowerCase();                       //Ставимо до нижнього регістру для порівняння.
+//     let isSpam = string.includes("spam") || string.includes("sale"); //Порівнюємо. Якщо в строці є spam or sale то true якщо ні то false.
+//     return isSpam;
+//   }
+
+// console.log(checkString("saedsdfvs"));
+// console.log(checkStringShortCode("sale"));
+
+//!============================================ Fourth Task ( Cycle While ) =============================
+function calcDays(depth) {
+    const speedDay = 7;
+    const speedNight = 2;
+    let totalDistance = 0;
+    let days = 0;
+    while(totalDistance < depth){     //Цикл while працює до тих пір поки не виконаються умова. По виконанню виходить з нього.
+        totalDistance += speedDay;
+        days += 1;
+        if(totalDistance < depth){
+            totalDistance -= speedNight;
+        }
+    }
+    console.log(days);
+}
+
+calcDays(42);
+calcDays(17);
+calcDays(18);
