2664e83b6de5655e5c75ca5309a87f02a629cae1
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-11-11 14:26:59 +0100

    123

--- js/Lesson_7_Callbacks_and_Arrow_functions.js
+++ js/Lesson_7_Callbacks_and_Arrow_functions.js
@@ -41,19 +41,90 @@
 // });
 
 //!========================================= Example (Cycle forEach) ================================================
-const cars = ["BMW", "Audi", "Mecedes", "Infiniti", "Ford", "Mazda"];
+// const cars = ["BMW", "Audi", "Mecedes", "Infiniti", "Ford", "Mazda"];
 
-for(let i = 0; i < cars.length; i++){   //?Приклад нумерації автомобілів з массиву циклом for!
-    console.log(`${i + 1}: ${cars[i]}`);
+// for(let i = 0; i < cars.length; i++){   //?Приклад нумерації автомобілів з массиву циклом for!
+//     console.log(`${i + 1}: ${cars[i]}`);
+// }
+
+// cars.forEach((car,i,cars) =>{ //? forEach приймає в якості аргумента коллбек функцію.Та він завжди доходить до кінця.Його не можна призупинити.
+//     console.log(`${i + 1}: ${car}`);
+// });
+
+// cars.forEach((car,i) =>{   //?Те ж саме що і попередня.
+//     console.log(`${i + 1}: ${car}`);
+// });
+
+// const callback = (car, i) => {  //?Якщо не написано третього параметру то значить що ми його не будемо використовувати(можна з 1 параметром)
+//     console.log(`${i + 1}: ${car}`);
+// };
+// cars.forEach(callback);         //?Теж саме що і попередні приклади.
+// //! Якщо вам треба щоб на якійсь ітерації масиву функція чи цикл зупинялись то forEach не підходить бо він іде завжди до кінця!!!
+
+//!=================================== Example (callback function and arrow function) ===============================================
+// function createProduct(obj, callback) {
+//   const newObject = { ...obj, id: Date.now() }; //? Date.now час від 1970 року до тепер(В мілісекундах)Тут ми створюємо новий такий самий obj
+//   callback(newObject);
+// }
+
+// function logProduct(product) {
+//   console.log(product);
+// }
+
+// function logTotalPrice({ amount, price }) {
+//   console.log(amount * price);
+// }
+
+// const product = {
+//     name: "chese",
+//     amount: 20,
+//     price: 100,
+// };
+
+// createProduct(product, logProduct);    //?Тут ми виводимо в консоль новий об'єкт.
+// createProduct(product, logTotalPrice); //?Тут ми виводимо в консоль добуток килькості на вартість. ДВА РІЗНИХ КОЛЛБЕКИ!!!
+
+// function createProductArrow(obj, callback) {
+//     const newObject = { ...obj, id: Date.now() };
+//     callback(newObject);
+//   }
+
+//   const logProductArrow = product => console.log(product);     //?Дві коллбек стрілочні функції. Аналогічні тим що вище.
+//   const logTotalPriceArrow = ({amount, price}) => console.log(amount * price);
+
+//   createProductArrow(product, logProductArrow);
+//   createProductArrow(product, logTotalPriceArrow);
+
+//!======================================= Example (Working Methods Under the Hood!) ==============================================
+// //?Робота методів під капотом!
+// const mass = [1,2,3,4,5];
+
+// function each(array, callback) {   //?Принцип роботи методу map під капотом.
+//     const newMass = [];
+//     for(let i = 0; i < array.length; i ++){     //?Теж саме можна зробити і циклом for of!!!
+//         const newElem = callback(array[i]);    //?Створили новий елемент в const і одразу викликаємо на ньому коллбек!
+//         console.log(newElem);
+//         newMass.push(newElem);
+//     }
+//     return newMass;
+// }
+
+// function pow(item) {
+//     return item * item;
+// }
+
+// console.log(each(mass, pow));
+// console.log(each(mass, (newElem) => newElem * newElem)); //?Стрілочна функція в консолі. Аналогічна функції pow.
+
+//!============================================== Example (forEach) ============================================
+// const array = [1,2,3,4,5,6,7,8,9];
+
+// let sum = 0; //?Накопичувач елементів масиву(тобто цифер)
+
+// array.forEach((el) => (sum += el));  //? forEach зроблений стрілочною функцією накопичує всі елементи масиву.
+// console.log(sum / array.length); //?В консолі ми одразу знаходимо середнє число масиву поділивши сумму на довжину масиву.
+
+//!============================================ Example (two callbacks) ====================================================
+const pizzaPalace = {
+    
 }
-
-cars.forEach((car,i,cars) =>{ //? forEach приймає в якості аргумента коллбек функцію.Та він завжди доходить до кінця.Його не можна призупинити.
-    console.log(`${i + 1}: ${car}`);
-});
-
-cars.forEach((car,i) =>{   //?Те ж саме що і попередня.
-    console.log(`${i + 1}: ${car}`);
-});
-//! Якщо вам треба щоб на якійсь ітерації масиву функція чи цикл зупинялись то forEach не підходить бо він іде завжди до кінця!!!
-
-//!========================================= Example ( ) ==================================================
