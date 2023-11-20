e4a9e14a3f565ba11ca3eb0f02cc8fcb0cd64765
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-11-20 21:20:33 +0100

    123

--- js/Lesson_9_Keyword_this.js
+++ js/Lesson_9_Keyword_this.js
@@ -18,34 +18,85 @@
 // console.log(chopChop.calcTotalPrice("Ruby"));
 
 //!============================================= Example (Method this) ===============================
-const phoneBook = {  //?Звичайний рефакторінг коду
-  contacts: [],
-  add(contact) {
-    const newContact = {
-      list: "default",
-      ...contact,
-      id: this.generateId(),     //? this посилається на об'єкт phoneBook тому що знизу коли викликається метод .add 
-      createdAt: this.getDate(),
-    };
-    this.contacts.push(newContact);
-  },
-  generateId() {
-    return "_" + Math.random().toString(36).substr(2, 9);
-  },
-  getDate() {
-    return Date.now();
-  },
-};
+// const phoneBook = {  //?Звичайний рефакторінг коду
+//   contacts: [],
+//   add(contact) {
+//     const newContact = {
+//       list: "default",
+//       ...contact,
+//       id: this.generateId(),     //? this посилається на об'єкт phoneBook тому що знизу коли викликається метод .add
+//       createdAt: this.getDate(),
+//     };
+//     this.contacts.push(newContact);
+//   },
+//   generateId() {
+//     return "_" + Math.random().toString(36).substr(2, 9);
+//   },
+//   getDate() {
+//     return Date.now();
+//   },
+// };
 
-phoneBook.add({                   //? Об'єкт стоїть phoneBook.
-        name: "Mango",
-        email:"mango@mail.com",
-        list: "friends",
-    });
+// phoneBook.add({                   //? Об'єкт стоїть phoneBook.
+//         name: "Mango",
+//         email:"mango@mail.com",
+//         list: "friends",
+//     });
 
-phoneBook.add({               //? Використання методу два рази.
-        name: "Poly",
-        email:"poly@hotmail.com",
-    });
+// phoneBook.add({               //? Використання методу два рази.
+//         name: "Poly",
+//         email:"poly@hotmail.com",
+//     });
 
-    console.log(phoneBook.contacts);   //?Вивід у консоль двух об'єктів так як два рази викликався метод!!!
+//     console.log(phoneBook.contacts);   //?Вивід у консоль двух об'єктів так як два рази викликався метод!!!
+
+//!=================================================== Example (Object methods and this ) ==========================================
+// calculator = {
+//     read(a = 0, b = 0){    //? a та b значення за замовчуванням якщо не буде присвоєно значення то буде 0.
+//         this.a = a;
+//         this.b = b;
+//     },
+//     add(){
+//         return this.a + this.b;
+//     },
+//     mult(){
+//         return this.a * this.b;
+//     },
+// };
+
+// calculator.read(10, 22);
+
+// console.log(calculator);
+
+// console.log(calculator.add());   //? щоб при додаванні чи множенні не було NaN а значення a та b не були undefined!
+// console.log(calculator.mult());
+
+// calculators = {
+//   read(a, b) {
+//     this.a = a || 0;    //? Або можна так поставити значення за замовчкуванням!
+//     this.b = b || 0;
+//   },
+//   add() {
+//     return this.a + this.b;
+//   },
+//   mult() {
+//     return this.a * this.b;
+//   },
+// };
+
+// calculators.read(10, 22);
+
+// console.log(calculators);
+
+// console.log(calculators.add());
+// console.log(calculators.mult());
+
+//!============================================= Example ( reduce and this ) ==============================================
+// function sumAll (arr) {
+//     return [...arr, ...this].reduce((sum, el) => sum + el, 0);  //?тут об'єднуємо два масиви в один і додаємо за допомогою reduce()
+// }
+
+// const arr1 = [1,2,3];
+// const arr2 = [4,5,6];
+
+// console.log(sumAll.apply(arr1, [arr2]));  //? apply() потрібен для того щоб вказати контекст і викликати функцію на місці.
