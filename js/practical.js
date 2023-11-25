5ab658317f38587d6ebd3814f3da12e2955f9944
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-11-25 21:36:27 +0100

    123

--- js/practical.js
+++ js/practical.js
@@ -1060,4 +1060,40 @@
 // let myRegex = /[^aeiou^0-9]/gi; // Change this line
 // let result = quoteSample.match(myRegex); // Change this line
 // console.log(result);
-//!================================================== Example 
+//!================================================== Example (All methods match()) ============================
+//?1.Користувачі можуть використовувати лише алфавітно-цифрові символи.
+//?2.Числа повинні бути лише в кінці імені користувача. У кінці їх може бути нуль або більше. Ім’я користувача не може починатися з числа.
+//?3.Літери в імені користувача можуть бути нижнього та верхнього регістру.
+//?4.Імена користувачів повинні містити щонайменше два символи. Ім’я користувача з двох символів може містити лише літери алфавіту.
+
+//?First example!!!
+// let username = "JackOfAllTrades";
+// let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
+// let result = userCheck.test(username);
+// console.log(result)
+
+//? ^ - start of input
+//? [a-z] - first character is a letter
+//? [a-z]+ - following characters are letters
+//? \d*$ - input ends with 0 or more digits
+//? | - or
+//? ^[a-z] - first character is a letter
+//? \d\d+ - following characters are 2 or more digits
+//? $ - end of input
+
+//?Second example!!!
+// let username = "JackOfAllTrades";
+// const userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i;
+// let result = userCheck.test(username);
+// console.log(result);
+
+//? ^ - start of input
+//? [a-z] - first character is a letter
+//? [0-9]{2,} - ends with two or more numbers
+//? | - or
+//? [a-z]+ - has one or more letters next
+//? \d* - and ends with zero or more numbers
+//? $ - end of input
+//? i - ignore case of input
+
+//!===================================================
