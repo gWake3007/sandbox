eaed1e5c281a79b102d959a367ab47c1b81e272f
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-12-05 22:38:14 +0100

    123

--- js/Study_for_Mentor_Lesson_3.js
+++ js/Study_for_Mentor_Lesson_3.js
@@ -2,17 +2,31 @@
 class User {
     #name;
     #surname;
-    constructor(name, surname){
+    static counter = 0;                //?Статична властивість. Завжди залишаються на классі і ніде не розповсюджуються!
+    static addUser() {                 //?Статичний метод.
+        this.counter += 1;              //?Лічильник юзерів!
+        console.log(`Кількість зареєстрованих користувачів ${this.counter}`);
+    }
+    constructor(name, surname, city){
         this.#name = name;
         this.#surname = surname;
         this.city = city;
+        User.addUser();           //?Викликається одразу в конструкторі класу та звертаємось до нього за назвою классу!!!
     }
     getFullName(){
         console.log(`${this.#name} ${this.#surname}`);
     }
+    changeName(newName) {           //?Приватна властивість зменюється тільки за допомогою методів(Для 6додаткових перевірок!)
+        if(this.#name === newName || newName.length < 3) {
+            return
+        }
+        this.#name = newName;
+    }
 }
 
-// const user = new User();
+// const user = new User("Петрик", "Петочкін");
+// user.getFullName();
+// user.changeName("afghj");
 // user.getFullName();
 
 class Student extends User {
@@ -23,13 +37,21 @@ class Student extends User {
     getCourse(){
         const currentYear = new Date().getFullYear();
         const studiesYears = currentYear - this.year;
-        console.log(studiesYears);
+        if(studiesYears < 5){
+            console.log(studiesYears);
+        } else {
+            console.log("Студен є випускником.");
+        }
+        
     }
 }
 
 const student = new Student("Петрик", "Петочкін", 2019);
 student.getCourse();
 student.getFullName();
+student.changeName("Vasilii");
+student.getFullName();
+const student1 = new Student("Serhii", "Reshetniak", 2022);
 
 // const currentDate = new Date();  //?Поточна дата з часовим поясом днем і часом.
 // const formattedDate = currentDate.toLocaleString();  //?Поточна дата.
@@ -38,3 +60,5 @@ student.getFullName();
 
 // const currentYear = new Date().getFullYear();    //?Поточний рік.
 // console.log(currentYear);
+
+//!================================================== Example ( ) ================================================
