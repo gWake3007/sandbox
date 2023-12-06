b1c0913430c787b75a11954600780202fb87af00
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-12-06 22:17:40 +0100

    123

--- js/Study_for_Mentor_Lesson_3.js
+++ js/Study_for_Mentor_Lesson_3.js
@@ -1,57 +1,57 @@
 //!===================================================== First Task (Methods class) ===============================================
-class User {
-    #name;
-    #surname;
-    static counter = 0;                //?Статична властивість. Завжди залишаються на классі і ніде не розповсюджуються!
-    static addUser() {                 //?Статичний метод.
-        this.counter += 1;              //?Лічильник юзерів!
-        console.log(`Кількість зареєстрованих користувачів ${this.counter}`);
-    }
-    constructor(name, surname, city){
-        this.#name = name;
-        this.#surname = surname;
-        this.city = city;
-        User.addUser();           //?Викликається одразу в конструкторі класу та звертаємось до нього за назвою классу!!!
-    }
-    getFullName(){
-        console.log(`${this.#name} ${this.#surname}`);
-    }
-    changeName(newName) {           //?Приватна властивість зменюється тільки за допомогою методів(Для 6додаткових перевірок!)
-        if(this.#name === newName || newName.length < 3) {
-            return
-        }
-        this.#name = newName;
-    }
-}
+// class User {
+//     #name;
+//     #surname;
+//     static counter = 0;                //?Статична властивість. Завжди залишаються на классі і ніде не розповсюджуються!
+//     static addUser() {                 //?Статичний метод.
+//         this.counter += 1;              //?Лічильник юзерів!
+//         console.log(`Кількість зареєстрованих користувачів ${this.counter}`);
+//     }
+//     constructor(name, surname, city){
+//         this.#name = name;
+//         this.#surname = surname;
+//         this.city = city;
+//         User.addUser();           //?Викликається одразу в конструкторі класу та звертаємось до нього за назвою классу!!!
+//     }
+//     getFullName(){
+//         console.log(`${this.#name} ${this.#surname}`);
+//     }
+//     changeName(newName) {           //?Приватна властивість зменюється тільки за допомогою методів(Для 6додаткових перевірок!)
+//         if(this.#name === newName || newName.length < 3) {
+//             return
+//         }
+//         this.#name = newName;
+//     }
+// }
 
 // const user = new User("Петрик", "Петочкін");
 // user.getFullName();
 // user.changeName("afghj");
 // user.getFullName();
 
-class Student extends User {
-    constructor(name,surname, year){
-        super(name, surname);
-        this.year = year;
-    }
-    getCourse(){
-        const currentYear = new Date().getFullYear();
-        const studiesYears = currentYear - this.year;
-        if(studiesYears < 5){
-            console.log(studiesYears);
-        } else {
-            console.log("Студен є випускником.");
-        }
+// class Student extends User {
+//     constructor(name,surname, year){
+//         super(name, surname);
+//         this.year = year;
+//     }
+//     getCourse(){
+//         const currentYear = new Date().getFullYear();
+//         const studiesYears = currentYear - this.year;
+//         if(studiesYears < 5){
+//             console.log(studiesYears);
+//         } else {
+//             console.log("Студен є випускником.");
+//         }
         
-    }
-}
+//     }
+// }
 
-const student = new Student("Петрик", "Петочкін", 2019);
-student.getCourse();
-student.getFullName();
-student.changeName("Vasilii");
-student.getFullName();
-const student1 = new Student("Serhii", "Reshetniak", 2022);
+// const student = new Student("Петрик", "Петочкін", 2019);
+// student.getCourse();
+// student.getFullName();
+// student.changeName("Vasilii");
+// student.getFullName();
+// const student1 = new Student("Serhii", "Reshetniak", 2022);
 
 // const currentDate = new Date();  //?Поточна дата з часовим поясом днем і часом.
 // const formattedDate = currentDate.toLocaleString();  //?Поточна дата.
@@ -61,4 +61,16 @@ const student1 = new Student("Serhii", "Reshetniak", 2022);
 // const currentYear = new Date().getFullYear();    //?Поточний рік.
 // console.log(currentYear);
 
-//!================================================== Example ( ) ================================================
+//!================================================== Example (Cycle for) ================================================
+const data = [1,2,3,4,5,6,7,8];  //Треба щоб массив виглядав так [[1,2],[3,4],[5,6],[7,8]]
+function foo(arr, size) {
+    const dataSort = [];
+    for(let i = 0; i < arr.length; i += size) {
+        const copy = arr.slice(i, i + size);
+        console.log(copy);
+        dataSort.push(copy);
+    }
+    console.log(dataSort);
+}
+
+foo(data, 3);
