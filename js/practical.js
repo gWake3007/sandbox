4dbfa15fdeb20a48649391b3e84facd90e14270a
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-12-19 14:52:07 +0100

    1

--- js/practical.js
+++ js/practical.js
@@ -1887,47 +1887,261 @@
 //     }
 //   }
 // })();
-//!=================================================== Example ( ) ========================================
+//!=================================================== Example (Objects .this and method splice() ) ========================================
 // вкладки – це масив заголовків до кожного сайту, відкритого у вікні
-const Window = function(tabs) {
-  this.tabs = tabs; // Ми зберігаємо записи про масив всередині об'єкта
+// const Window = function(tabs) {
+//   this.tabs = tabs; // Ми зберігаємо записи про масив всередині об'єкта
+// };
+
+// // Коли ви об'єднуєте два вікна в одне
+// Window.prototype.join = function(otherWindow) {
+//   this.tabs = this.tabs.concat(otherWindow.tabs);
+//   return this;
+// };
+
+// // Коли ви відкриєте нову вкладку в кінці
+// Window.prototype.tabOpen = function(tab) {
+//   this.tabs.push('new tab'); // Давайте поки відкриємо нову вкладку
+//   return this;
+// };
+
+// // Коли ви закриєте вкладку
+// Window.prototype.tabClose = function(index) {
+
+//   // Змініть код лише під цим рядком
+
+//   const tabsBeforeIndex = this.tabs.splice(0, index); // Отримайте вкладки перед вкладкою
+//   const tabsAfterIndex = this.tabs.splice(1); // Отримайте вкладки після вкладки
+// console.log(tabsBeforeIndex);
+// console.log(tabsAfterIndex);
+//   this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // З'єднайте їх разом
+// // console.log(this.tabs);
+//   // Змініть код лише над цим рядком
+
+//   return this;
+//  };
+
+// // Давайте створимо три вікна браузера
+// const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Ваша поштова скринька, диск та інші робочі сторінки
+// const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Соціальні сторінки
+// const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Розважальні сайти
+
+// // Тепер виконайте закриття, відкриття вкладки та інші операції
+// const finalTabs = socialWindow
+//   .tabOpen() // Відкрийте нову вкладку для мемів з котами
+//   .join(videoWindow.tabClose(2)) // Закрийте третю вкладку у вікні відео та приєднайтеся
+//   .join(workWindow.tabClose(1).tabOpen());
+// console.log(finalTabs.tabs);
+// //?Зверни увагу як працює метод splice() з одним аргументом. Він вирізає від цього індекса і до кінця!!!
+// const arrr = ['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp'];
+// console.log(arrr);
+// const arr1 = arrr.splice(1);
+// console.log(arr1);
+
+//!========================================= Example (Remove && add item) ======================================================
+//? Заміна змінних без мутування глобальної змінної
+// const bookList = [
+//   "The Hound of the Baskervilles",
+//   "On The Electrodynamics of Moving Bodies",
+//   "Philosophiæ Naturalis Principia Mathematica",
+//   "Disquisitiones Arithmeticae",
+// ];
+
+// //? Для додавання книг до массиву.
+// function add(bookList,bookName) {
+//   const item = [...bookList];
+//   item.push(bookName);
+//   return item;
+// }
+
+// //?Для заберання потрібної нам книги з массиву.
+// function remove(bookList,bookName) {
+//   const itemm = [...bookList];
+//   const book_index = itemm.indexOf(bookName);
+//   if (book_index >= 0) {
+//     itemm.splice(book_index, 1);
+//     return itemm;
+//   }
+// }
+// console.log(add(bookList, "A Brief History of Time"));
+// console.log(remove(bookList, "On The Electrodynamics of Moving Bodies"));
+// console.log(remove(add(bookList, "A Brief History of Time"), "On The Electrodynamics of Moving Bodies"));
+// console.log(bookList);  //?Тут ми бачимо що основний масив не мутується.
+//!========================================== Example (Cycle for and callback map() && filter() ) =========================================
+//?Переробимо функцію з циклу for 
+// const watchList = [
+//   {
+//     "Title": "Inception",
+//     "Year": "2010",
+//     "Rated": "PG-13",
+//     "Released": "16 Jul 2010",
+//     "Runtime": "148 min",
+//     "Genre": "Action, Adventure, Crime",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Christopher Nolan",
+//     "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+//     "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+//     "Language": "English, Japanese, French",
+//     "Country": "USA, UK",
+//     "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//     "Metascore": "74",
+//     "imdbRating": "8.8",
+//     "imdbVotes": "1,446,708",
+//     "imdbID": "tt1375666",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "Interstellar",
+//     "Year": "2014",
+//     "Rated": "PG-13",
+//     "Released": "07 Nov 2014",
+//     "Runtime": "169 min",
+//     "Genre": "Adventure, Drama, Sci-Fi",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Jonathan Nolan, Christopher Nolan",
+//     "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+//     "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+//     "Language": "English",
+//     "Country": "USA, UK",
+//     "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+//     "Metascore": "74",
+//     "imdbRating": "8.6",
+//     "imdbVotes": "910,366",
+//     "imdbID": "tt0816692",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "The Dark Knight",
+//     "Year": "2008",
+//     "Rated": "PG-13",
+//     "Released": "18 Jul 2008",
+//     "Runtime": "152 min",
+//     "Genre": "Action, Adventure, Crime",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+//     "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+//     "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+//     "Language": "English, Mandarin",
+//     "Country": "USA, UK",
+//     "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+//     "Metascore": "82",
+//     "imdbRating": "9.0",
+//     "imdbVotes": "1,652,832",
+//     "imdbID": "tt0468569",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "Batman Begins",
+//     "Year": "2005",
+//     "Rated": "PG-13",
+//     "Released": "15 Jun 2005",
+//     "Runtime": "140 min",
+//     "Genre": "Action, Adventure",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+//     "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+//     "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+//     "Language": "English, Urdu, Mandarin",
+//     "Country": "USA, UK",
+//     "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+//     "Metascore": "70",
+//     "imdbRating": "8.3",
+//     "imdbVotes": "972,584",
+//     "imdbID": "tt0372784",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "Avatar",
+//     "Year": "2009",
+//     "Rated": "PG-13",
+//     "Released": "18 Dec 2009",
+//     "Runtime": "162 min",
+//     "Genre": "Action, Adventure, Fantasy",
+//     "Director": "James Cameron",
+//     "Writer": "James Cameron",
+//     "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+//     "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+//     "Language": "English, Spanish",
+//     "Country": "USA, UK",
+//     "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+//     "Metascore": "83",
+//     "imdbRating": "7.9",
+//     "imdbVotes": "876,575",
+//     "imdbID": "tt0499549",
+//     "Type": "movie",
+//     "Response": "True"
+//   }
+// ];
+//?Метод filter() щоб дістати ті ж самі елементи тільки вже з рейтингом 8.0 або вище!А методом map() вже виводимо потрібні поля об'єктів
+// const filteredList = watchList.filter(item => Number(item.imdbRating) >= 8).map(item => {
+//   return {title:item.Title, rating:item.imdbRating};
+// });
+
+// console.log(filteredList);
+
+//?Два альтернативні скрипта для отримання двух значеннь з масиву об'єктів
+
+// const ratings = [];
+// for (let i = 0; i < watchList.length; i++) {
+//   ratings.push({title: watchList[i]["Title"], rating: watchList[i]["imdbRating"]});
+// }
+//?ЗВЕРНИ УВАГУ!!! Як можна через map() дістати більше одного значення!!!
+// const ratings = watchList.map(item => {
+//  return {title:item.Title, rating:item.imdbRating};
+// });
+
+// console.log(JSON.stringify(ratings));
+//!============================================ Example (Alternative method map() => Cycle for & forEach) ================================
+//?Альтернатива написання методу map() звичайним циклом for.
+// Array.prototype.myMap = function(callback) {
+//   const newArray = [];
+//   for(let i = 0; i < this.length; i++) {
+//     newArray.push(callback(this[i],i,this));  //?Тут ми прописуємо всі три аргумента методу map(Сам елемент,індекс цього елементу,массив)
+//   }
+//   return newArray;
+// };
+//?Альтернативне написання методу map() через forEach
+// Array.prototype.myMap = function (callback) {
+//   const newArray = [];
+//   this.forEach((element, index, originalArr) =>
+//     newArray.push(callback(element, index, originalArr))
+//   );
+//   return newArray;
+// };
+//?Альтернативний метод .map() вже фактично написаний в консолі.
+// console.log([23, 65, 98, 5, 13].myMap(item => item * 2));
+// console.log(["naomi", "quincy", "camperbot"].myMap(element => element.toUpperCase()));
+// console.log([1, 1, 2, 5, 2].myMap((element, index, array) => array[index + 1] || array[0]));
+//!================================================== Example (Alternative methods filter() ) ======================================
+//?Альтернатива методу filter() циклом for і розгалудженням з умовою колбеку.
+// Array.prototype.myFilter = function(callback) {
+//   const newArray = [];
+//   for(let i = 0; i < this.length; i ++) {
+//     if(callback(this[i], i, this)){
+//       newArray.push(this[i]);
+//     }
+//   }
+//   return newArray;
+// };
+//?Альтернатива методу filter() методом forEach.
+Array.prototype.myFilter = function(callback) {
+  const newArray = [];
+  this.forEach((item, index, array) => {
+ callback(item, index, array) ?
+  newArray.push(item)
+});
+  return newArray;
 };
 
-// Коли ви об'єднуєте два вікна в одне
-Window.prototype.join = function(otherWindow) {
-  this.tabs = this.tabs.concat(otherWindow.tabs);
-  return this;
-};
-
-// Коли ви відкриєте нову вкладку в кінці
-Window.prototype.tabOpen = function(tab) {
-  this.tabs.push('new tab'); // Давайте поки відкриємо нову вкладку
-  return this;
-};
-
-// Коли ви закриєте вкладку
-Window.prototype.tabClose = function(index) {
-
-  // Змініть код лише під цим рядком
-
-  const tabsBeforeIndex = this.tabs.splice(0, index); // Отримайте вкладки перед вкладкою
-  const tabsAfterIndex = this.tabs.splice(index + 1); // Отримайте вкладки після вкладки
-
-  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // З'єднайте їх разом
-
-  // Змініть код лише над цим рядком
-
-  return this;
- };
-
-// Давайте створимо три вікна браузера
-const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Ваша поштова скринька, диск та інші робочі сторінки
-const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Соціальні сторінки
-const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Розважальні сайти
-
-// Тепер виконайте закриття, відкриття вкладки та інші операції
-const finalTabs = socialWindow
-  .tabOpen() // Відкрийте нову вкладку для мемів з котами
-  .join(videoWindow.tabClose(2)) // Закрийте третю вкладку у вікні відео та приєднайтеся
-  .join(workWindow.tabClose(1).tabOpen());
-console.log(finalTabs.tabs);
+console.log([23, 65, 98, 5, 13].myFilter(item => item % 2));
+console.log(["naomi", "quincy", "camperbot"].myFilter(element => element === "naomi"));
+console.log([1, 1, 2, 5, 2].myFilter((element, index, array) => array.indexOf(element) === index));
