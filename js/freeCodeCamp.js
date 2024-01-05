4999cd7a9ba6e45a0a500564fb8050f65aff667e
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-01-05 10:46:43 +0100

    ё

--- js/freeCodeCamp.js
+++ js/freeCodeCamp.js
@@ -3290,9 +3290,90 @@
 
 // const originalAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 // console.log(originalAlphabet.charCodeAt(26));   //?Принцип роботи команди .charCodeAt() всередині індекс елемента!!!
-//!=============================================== Example 
-function telephoneCheck(str) {
-  
-}
+//!=============================================== Example
+// function telephoneCheck(str) {
+//   const phoneNumbers = [
+//     "555-555-5555",
+//     "(555)555-5555",
+//     "(555) 555-5555",
+//     "555 555 5555",
+//     "5555555555",
+//   ];
+//   const phoneNumbersOne = [
+//     "1 555 555 5555",
+//     "1 555-555-5555",
+//     "1 (555) 555-5555",
+//     "1(555)555-5555",
+//   ]; 
+//   if(str.length >= 14 && str[0].includes(1)) {
+//     return "phoneNumbersOne";
+//   } else {
+//     return "phoneNumbers";
+//   }
 
-console.log(telephoneCheck("555-555-5555"));
+//   return str;
+// }
+
+// console.log(telephoneCheck("555-555-5555"));
+// console.log(telephoneCheck("1(555)555-5555"));
+
+//   const phoneNumbersOne = [   //! На все воно вказує що це string.Це треба поправити!!!!Перетворишви в Number!!!
+//     "1 555 555 5555",
+//     "1 555-555-5555",
+//     "1 (555) 555-5555",
+//     "1(555)555-5555",
+//   ]; 
+
+// console.log(typeof phoneNumbersOne[0][0]);
+// console.log(phoneNumbersOne[0][0]);
+
+// let oneNumber = "1 555 555 5555";
+// console.log(typeof oneNumber);
+// // console.log(typeof Number(oneNumber));
+// oneNumber = Number(oneNumber);
+// console.log(typeof oneNumber);
+// console.log(oneNumber);   //! Почитай тут чому воно саме так? За лапок чи ще чогось
+
+// let twoNumber = 1 555 555 5555;
+// let threeNumber = 1(555)555-5555;  сразу цифрой не пойдёт.Нужно искать способ поменять стрингу на номер!!
+
+
+// function arr(arrr) {
+//   for(let i = 0; i < arrr.length; i++) {
+//     console.log(phoneNumbersOne[i]);
+//     for(let k = 0; k < phoneNumbersOne[i].length; k++) {
+//       console.log(k);
+//       //?Умова що перший елемент САМЕ В ЦЬОМУ МАСИВІ 1. далі або пробіл або дужка. далі три numbers далі пробіл тире або дужка(якщо дужка то )
+//       //?мож бути і пробіл і набер і тд. ПОДУМАЙ ЯК ЗАДАЧУ РОЗДІЛИТИ????
+//     }
+//   }
+// }
+
+// console.log(arr(phoneNumbersOne));
+
+
+
+
+
+
+
+
+// typeOf з кожною цифрою можна зробити.
+// const phoneNumbersOne = [
+//   "1 555 555 5555",
+//   "1 555-555-5555",
+//   "1 (555) 555-5555",
+//   "1(555)555-5555",
+// ]; 
+
+// console.log(phoneNumbersOne[3].length);  //14 14 16 14
+
+  // const phoneNumbers = [
+  //   "555-555-5555",
+  //   "(555)555-5555",
+  //   "(555) 555-5555",
+  //   "555 555 5555",
+  //   "5555555555",
+  // ];
+
+  // console.log(phoneNumbers[4].length);   // 12 13 14 12 10
