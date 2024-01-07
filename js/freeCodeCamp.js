4b24a8ccb0d0bf756409af2c710ead710fc7b156
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-01-07 22:25:49 +0100

    123

--- js/freeCodeCamp.js
+++ js/freeCodeCamp.js
@@ -3307,18 +3307,76 @@
 //!======================================================= Example
 //?Задача касовий апарат:
 function checkCashRegister(price, cash, cid) {
-  let change;
-  return change;
+  const allCashCosts = {
+    PENNY: 0.01,
+    NICKEL: 0.05,
+    DIME: 0.1,
+    QUARTER: 0.25,
+    ONE: 1,
+    FIVE: 5,
+    TEN: 10,
+    TWENTY: 20,
+    "ONE HUNDRED": 100,
+  };
+  let change = cid
+    .flatMap((element) => element[1])
+    .reduce((accum, element) => accum + element, 0);
+  return change < cash - price
+    ? { status: "INSUFFICIENT_FUNDS", change: [] }
+    : {
+        status: "CLOSED",
+        change: [
+          ["PENNY", 0.5],
+          ["NICKEL", 0],
+          ["DIME", 0],
+          ["QUARTER", 0],
+          ["ONE", 0],
+          ["FIVE", 0],
+          ["TEN", 0],
+          ["TWENTY", 0],
+          ["ONE HUNDRED", 0],
+        ],
+      };
 }
+//? Тут треба придумати як зробити так щоб повертало кожен раз різний масив який в умові при клозед а також перевіряло чи можна дати здачу нормально.
+console.log(
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
 
-checkCashRegister(19.5, 20, [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100],
-]);
+console.log(
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 0.01],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 1],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0],
+  ])
+);
+
+console.log(
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 0.5],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0],
+  ])
+);
