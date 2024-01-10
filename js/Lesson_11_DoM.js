7a3fe5afbf697b6844491ffa6e0ad6100fcf22e5
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-01-10 21:44:11 +0100

    1

--- js/Lesson_11_DoM.js
+++ js/Lesson_11_DoM.js
@@ -10,10 +10,34 @@
 // list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
 //!============================================= First task =============================================================================
 const textByID = document.getElementById("text");      //?Дві альтернативи як достукатись до елемента в ДОМ.
-console.log(textByID);
+// console.log(textByID);
 const textSelector = document.querySelector("#text");
-console.log(textSelector);
-console.dir(textSelector);                         //?У вигляді об'єкта.
+// console.log(textSelector);
+// console.dir(textSelector);                         //?У вигляді об'єкта.
 textByID.textContent = "Change text for textContent";    //?Зміна тексту через Js.
 textByID.style.backgroundColor = "Orange";               //?Зміна стилів через Js.
 textByID.classList.add("text");                          //?Додає всі зміни в html тезі style.
+// text.remove();                                        //?Видалення параграфу з id text!!!
+textByID.innerHTML = "";//?Альтернативний спосіб видалення(Можна так робити якщо в ul чи div де є елементи але цей метод або видаляє чи замінює)
+
+const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+
+const listId = document.getElementById("technologiesList");
+console.log(listId);
+
+// const markupMap = technologies              //?Створювання елементів li через map + join
+//   .map((technology) => `<li class="item-technology">${technology}</li>`)
+//   .join("");
+// listId.insertAdjacentHTML("afterbegin", markupMap);   //?Додавання цієї розмітки в ul в HTML документі.
+
+const markupReduce = technologies.reduce((accum, item) => accum + `<li class="item-technology">${item}</li>`, "");
+
+listId.insertAdjacentHTML("afterbegin", markupReduce);
+
+const pElement = document.createElement("p"); //?Створюємо параграф всередині Java Script
+pElement.className = "paragraph";  //?Додаємо класс цьому параграфу.
+pElement.textContent = "Я створений параграф всередині Java Script!";
+
+technologiesList.append(pElement);    //?Додаємо до нашого ul цей створений параграф!
+
+
