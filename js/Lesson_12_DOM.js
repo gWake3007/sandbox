463eace779557b5bdd88d1821d5a6bf459e1f7fd
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-01-13 09:58:03 +0100

    123

--- js/Lesson_12_DOM.js
+++ js/Lesson_12_DOM.js
@@ -62,22 +62,82 @@
 //   console.log("click event listener was removed from btn");
 // });
 //!=================================================== Example ( bind() ) ===================================================================
-//? Приклад в якій концепції працює слухач з концепцією this.
-const mango = {
-    username: "Mango",
-    showUsername() {
-      console.log(this);
-      console.log(`My username is: ${this.username}`);
-    },
-  };
+//? Приклад в якій концепції працює слухач з концепцією this. Для того щоб this працював зі слухачем чи якоюсь другою функцією 
+//?використовуємо метод bind().
+// const mango = {
+//     username: "Mango",
+//     showUsername() {
+//       console.log(this);
+//       console.log(`My username is: ${this.username}`);
+//     },
+//   };
   
-  const btn = document.querySelector(".js-btn");
+//   const btn = document.querySelector(".js-btn");
   
-  // ✅ Працює
-  mango.showUsername();
+//   // ✅ Працює
+//   mango.showUsername();
   
-  // ❌ this буде посилатися на button, якщо використовувати showUsername як callback
-  btn.addEventListener("click", mango.showUsername); // не працює
+//   // ❌ this буде посилатися на button, якщо використовувати showUsername як callback
+//   btn.addEventListener("click", mango.showUsername); // не працює  //Тут буде undefined!!!
   
-  // ✅ Не забувайте прив'язувати контекст методів об'єкта
-  btn.addEventListener("click", mango.showUsername.bind(mango));
+//   // ✅ Не забувайте прив'язувати контекст методів об'єкта
+//   btn.addEventListener("click", mango.showUsername.bind(mango));
+//!============================================== Example( event, event type , currentTarget ) ===============================================
+// const button = document.querySelector(".btn");
+
+// const handleClick = (event) => {
+//   console.log("event: ", event);       //?Вказує на сам евент.
+//   console.log("event type: ", event.type);    //?Вказує на тип евенту.
+//   console.log("currentTarget: ", event.currentTarget);    //?Вказує на потойний таргет(елемент DOM дерева!!!)
+// };
+
+// button.addEventListener("click", handleClick);
+//!=============================================== Example ( preventDefault ) ===========================================================
+//?Робота з input через form. Добираємся до селектора форми. Потім через слухача та деструкторизацію до інпутів і виводимо введенні значення.
+// const form = document.querySelector(".register-form");
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault(); //? preventDefault - потрібен для того щоб скасувати дії браузера за замовчкуванням(НЕ БАЖАНІ ДІЇ БРАУЗЕРА)
+//   const {
+//     elements: { username, password }     //?Спосіб деструкторизувати input в form. Звертається до назв input( NAME ) ВАЖЛИВО!!!
+//   } = event.currentTarget;
+//   console.log(username.value, password.value);
+// });
+//!===================================================== Example ( keydown , keyup) ========================================================
+//? Відслідковування натискання і відтискання клавіши.
+// const clearLogBtn = document.querySelector(".js-clear");
+// const logList = document.querySelector(".log-list");
+// let keypressCounter = 1;
+
+// console.log(clearLogBtn)
+
+// document.addEventListener("keydown", logMessage);
+// document.addEventListener("keyup", logMessage);
+// clearLogBtn.addEventListener("click", reset);
+
+// function logMessage({ type, key, code }) {
+//   const markup = `<div class="log-item">
+//     <span class="chip">${keypressCounter}</span>
+//     <ul>
+//       <li><b>Event</b>: ${type}</li>
+//       <li><b>Key</b>: ${key}</li>
+//       <li><b>Code</b>: ${code}</li>
+//     </ul>
+//   </div>`;
+
+//   logList.insertAdjacentHTML("afterbegin", markup);
+
+//   if (type === "keyup") {
+//     incrementKeypressCounter();
+//   }
+// }
+
+// function reset() {
+//   keypressCounter = 1;
+//   logList.innerHTML = "";
+// }
+
+// function incrementKeypressCounter() {
+//   keypressCounter += 1;
+// }
+//!======================================================== Example 
