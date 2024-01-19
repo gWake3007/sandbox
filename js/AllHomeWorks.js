9753619581d2334a4598a365d4af3a7989fab6ef
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-01-19 10:16:29 +0100

    123

---
+++ js/AllHomeWorks.js
@@ -0,0 +1,8 @@
+//!======================================================= Task number one ==========================================================
+const categories = document.getElementById("categories");
+const cat = categories.split("");
+// console.log(cat);
+
+// cat.forEach(function(element, index, array) {
+//     console.log(element);
+// });
