8133586bb054cdf527bac4832636efef667fecc2
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-01-24 23:47:18 +0100

    1

---
+++ js/Lesson_14_trottle_debounce_&_lazy_load.js
@@ -0,0 +1,10 @@
+//!================================================= First example ===============================================================
+//? Event "scroll" - спрацьовує при скролингу мишкою.
+// const output = document.querySelector(".output");
+// let scrollEventCounter = 0;
+
+// document.addEventListener("scroll", () => {
+//   scrollEventCounter += 1;
+//   output.textContent = scrollEventCounter;
+// });
+//!================================================= Second example ============================================================
