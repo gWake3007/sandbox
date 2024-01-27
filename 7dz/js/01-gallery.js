e9eae2ea50ed0957f768530b1c5442521f00a1b5
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-01-27 14:08:42 +0100

    1

--- 7dz/js/01-gallery.js
+++ 7dz/js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (item) => `<li class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a>
+</li>`
+  )
+  .join("");
+
+gallery.innerHTML = markup;
+
+gallery.addEventListener("click", openModal);
+
+function openModal(event) {
+  event.preventDefault();
+  const clickTag = event.target;
+  if (clickTag.tagName === "IMG") {
+    const instance = basicLightbox.create(`
+    <img
+    src="${event.target.dataset.source}"
+  />`);
+    instance.show();
+    document.body.addEventListener("keydown", pressEscape);
+    function pressEscape(event) {
+      if (event.code === "Escape") {
+        instance.close();
+      }
+    }
+  }
+}
--- 7dz/js/02-lightbox.js
+++ 7dz/js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`,
+  ""
+);
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", handleClick);
+
+function handleClick(event) {
+    event.preventDefault();
+    const galleryA = new SimpleLightbox (".gallery a", {
+        captions: true,
+        captionDelay: 250,
+        captionPosition: 'bottom',
+        captionsData: 'alt',
+    });
+}
