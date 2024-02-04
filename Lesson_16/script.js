ace94540d40478ed505d1b136d9c5c985331cb1a
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-02-04 13:54:06 +0100

    1

--- Lesson_16/script.js
+++ Lesson_16/script.js
@@ -1,11 +1,20 @@
+import { createMarkup } from "./teamplates/product.js";  //?Тут імпортуємо функцію розмітки.
 import instruments from "./products.json" assert { type: "json" };
  //?Імпортуємо масив об'єктів з json файлі(без ексорту тому що це json!). А також вказуємо командою assert що це файл json!
  //? instruments - це теж саме що const для файла у самому Java Script щоб він сприймався як масив.Щоб з ним працювати.
-console.log(instruments);
 
 const refs = {
     productsList: document.getElementById("products-container-id"),
+    product: document.getElementById("js-product"),
+    button: document.getElementById("js-add"),
 };
 
 const PRODUCTS_LS_KEY = "checkout";
 
+refs.productsList.insertAdjacentHTML("beforeend", createMarkup(instruments));
+
+refs.productsList.addEventListener("click", onClick);
+
+function onClick(event) {
+    console.log(event.target.id);
+}
