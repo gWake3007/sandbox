a9eb0f58c86b747ad0339d503e8b211931e492d7
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-03-22 20:49:07 +0100

    1

---
+++ js/Study_for_Mentor_Lesson_6.js
@@ -0,0 +1,39 @@
+const URL = "https://rickandmortyapi.com/api/character/";
+
+const list = document.querySelector(".list-rik-morty");
+const btn_loadMore = document.querySelector(".loan-more-js");
+let page = 1;
+
+function serviceCharacter() {
+  return fetch(`${URL}?page=${page}`).then((resp) => {
+    if (!resp.ok) {
+      throw new Error(resp.status);
+    }
+    return resp.json();
+  });
+}
+
+serviceCharacter()
+  .then((data) => createMarkup(data.results))
+  .catch((err) => console.log(err));
+
+function createMarkup(data) {
+  const markup = data
+    .map(
+      ({ image, name }) => `<li class="item-rik-morty">
+    <img src="${image}" alt="${name}" class="img-rik-morty">
+    <h2 class="title-rik-morty">${name}</h2>
+</li>`
+    )
+    .join("");
+  list.insertAdjacentHTML("beforeend", markup);
+}
+
+btn_loadMore.addEventListener("click", loadMore);
+
+function loadMore() {
+  page += 1;
+  serviceCharacter()
+    .then((data) => createMarkup(data.results))
+    .catch((err) => console.log(err));
+}
