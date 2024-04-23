fff8b940a661d5da95b86399fbfe7975c67a98cc
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-04-23 10:33:08 +0200

    123

--- adaptive_burger_menu/main.js
+++ adaptive_burger_menu/main.js
@@ -28,10 +28,11 @@ const isMobile = {
   },
 };
 
+//?Селектор арров
+let menuArrows = document.querySelectorAll(".menu-arrow");
 //?Перевіряє чи це мобільний пристрій чи ПК та при клікі якщо це мобільний пристрій додає класс active для css.
 if (isMobile.any()) {
   document.body.classList.add("touch");
-  let menuArrows = document.querySelectorAll(".menu-arrow");
   if (menuArrows.length > 0) {
     for (let i = 0; menuArrows.length > i; i++) {
       const menuArrow = menuArrows[i];
@@ -43,6 +44,7 @@ if (isMobile.any()) {
 } else {
   document.body.classList.add("pc");
 }
+
 //?Меню бургер
 const iconMenu = document.querySelector(".menu-icon");
 const menuBody = document.querySelector(".menu-body");
@@ -52,12 +54,16 @@ if (iconMenu) {
     document.body.classList.toggle("lock");
     iconMenu.classList.toggle("active");
     menuBody.classList.toggle("active");
+    auditSubMenu(menuArrows, iconMenu);
   });
 }
 
 //?Всі лінки з классом і дата атрибутом.
 const menuLinks = document.querySelectorAll(".menu-link[data-goto]");
 const menuSubLinks = document.querySelectorAll(".menu-sub-link[data-goto]");
+
+//?Лінк саб меню.
+const menuSubList = document.querySelector(".menu-sub-list");
 
 if (menuLinks.length > 0) {
   menuLinks.forEach((menuLink) => {
@@ -79,51 +85,69 @@ if (menuLinks.length > 0) {
         pageYOffset -
         document.querySelector("header").offsetHeight;
       //?Перевіряємо чи натиснуто в бургер меню по навігації.Якщо так то закриваємо бургер меню і проскролює до цієї частини сторінки.
-      if (iconMenu.classList.contains("active")) {
-        document.body.classList.remove("lock");
-        iconMenu.classList.remove("active");
-        menuBody.classList.remove("active");
-      }
-
-      window.scrollTo({
-        top: gotoBlockValue,
-        behavior: "smooth",
-      });
+      checkOpenMobileMenu(iconMenu, menuBody);
+      smoothSkroll(gotoBlockValue);
       //?Для відключення роботи посилання та корректної роботи скрипта!ВАЖЛИВО!!!
       event.preventDefault();
     }
   }
+  //?Евент для кліків по саб меню.
   if (menuSubLinks.length > 0) {
     menuSubLinks.forEach((menuSubLink) => {
       menuSubLink.addEventListener("click", onMenuSubLinkClick);
     });
-//?Функція для навігації по вкладкі в бургер меню.ВАЖЛИВО!!!
+
+    //?Функція для навігації по вкладкі в бургер меню.ВАЖЛИВО!!!
     function onMenuSubLinkClick(event) {
       const menuSubLink = event.target;
+
       if (
         menuSubLink.dataset.goto &&
         document.querySelector(menuSubLink.dataset.goto)
       ) {
-              //?Отримуємо селектор лінка на який натиснуто.
-      const gotoBlockSub = document.querySelector(menuSubLink.dataset.goto);
-      //?Отримуємо висоту елемента мінус висота хедера(Тому що хеддер в нас fixed та пересувається з контентом).
-      const gotoBlockValue =
-      gotoBlockSub.getBoundingClientRect().top +
-        pageYOffset -
-        document.querySelector("header").offsetHeight;
-        if (iconMenu.classList.contains("active")) {
-          document.body.classList.remove("lock");
-          iconMenu.classList.remove("active");
-          menuBody.classList.remove("active");
-        }
-
-        window.scrollTo({
-          top: gotoBlockValue,
-          behavior: "smooth",
-        });
+        //?Отримуємо селектор лінка на який натиснуто.
+        const gotoBlockSub = document.querySelector(menuSubLink.dataset.goto);
+        //?Отримуємо висоту елемента мінус висота хедера(Тому що хеддер в нас fixed та пересувається з контентом).
+        const gotoBlockValue =
+          gotoBlockSub.getBoundingClientRect().top +
+          pageYOffset -
+          document.querySelector("header").offsetHeight;
+          checkOpenMobileMenu(iconMenu, menuBody);
+        smoothSkroll(gotoBlockValue);
         //?Для відключення роботи посилання та корректної роботи скрипта!ВАЖЛИВО!!!
         event.preventDefault();
       }
     }
   }
+}
+
+//?Плавний скрол до потрібного відділу сайту.
+function smoothSkroll(gotoBlockValue) {
+  return window.scrollTo({
+    top: gotoBlockValue,
+    behavior: "smooth",
+  });
+}
+
+//?Функція для перевірки кількості стрілок(якщо їх більше однієї) і для закриття всіх саб меню при закритті меню, при переході по посиланні в меню чи саб меню.
+function auditSubMenu(menuArrows, iconMenu) {
+  if (menuArrows.length > 0) {
+    for (let i = 0; menuArrows.length > i; i++) {
+      const menuArrow = menuArrows[i];
+      if (!iconMenu.classList.contains("active")) {
+        console.log("audit");
+        menuArrow.parentElement.classList.remove("active");
+      }
+    }
+  }
+}
+
+//?Функція для перевірки чи натиснуто в бургер меню по навігації.Якщо так то закриваємо бургер меню.
+function checkOpenMobileMenu(iconMenu, menuBody) {
+    if (iconMenu.classList.contains("active")) {
+        document.body.classList.remove("lock");
+        iconMenu.classList.remove("active");
+        menuBody.classList.remove("active");
+        auditSubMenu(menuArrows, iconMenu);
+      }
 }
