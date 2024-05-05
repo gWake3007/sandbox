a4fcb232f82f98e044acfd88db957ebb332ac77f
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-05-05 10:08:08 +0200

    1

---
+++ Form_Validation_&_Buttons_Example/main.js
@@ -0,0 +1,8 @@
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", submitAndValidationForm);
+
+function submitAndValidationForm(event) {
+    event.preventDefault();
+    
+}
