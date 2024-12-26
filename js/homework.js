a77aa213e3c0d3c3e9eb99a1ffe4b93a4c33fbab
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-12-26 22:11:15 +0100

    123

--- js/homework.js
+++ js/homework.js
@@ -7,11 +7,22 @@
 // console.log(message);
 
 // Change code below this line
-function add(a, b, c) {
-  console.log("Addition result equals ${a+b+c}");
-  // Change code above this line
+// function add(a, b, c) {
+//   console.log("Addition result equals ${a+b+c}");
+//   // Change code above this line
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+//?geometricProgression
+function geometricProgression(num, howToProgress) {
+  let result = num;
+  for (let i = 1; i < howToProgress; i++) {
+    result = result * 2;
+    // console.log(result);
+  }
+  console.log(result);
 }
 
-add(15, 27, 10);
-add(10, 20, 30);
-add(5, 10, 15);
+geometricProgression(1, 31);
--- train/index.js
+++ train/index.js
@@ -1,1 +1,45 @@
+import * as Yup from "yup";
 //?            Validation
+const nameValidation = (value) => {
+  const nameRegex = /^[a-zA-Z]+$/;
+  const parts = value.split(" ");
+  if (parts.length !== 2) {
+    return "Please enter both first and last name";
+  }
+  if (!nameRegex.test(parts[0]) || !nameRegex.test(parts[1])) {
+    return "Both first and last name should only contain letters";
+  }
+  if (parts[0].length < 3 || parts[1].length < 3) {
+    return "Both first and last name should be at least 3 characters long";
+  }
+  if (parts[0].length > 50 || parts[1].length > 50) {
+    return "Both first and last name should be no longer than 50 characters";
+  }
+  if (
+    parts[0][0] !== parts[0][0].toUpperCase() ||
+    parts[1][0] !== parts[1][0].toUpperCase()
+  ) {
+    return "Both first and last name should start with a capital letter";
+  }
+  return true;
+};
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .test(
+      "is-full-name",
+      "User name is not valid. Format should be FirstName LastName",
+      (value) => nameValidation(value) === true
+    )
+    .required("Required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .matches(
+      /^(?=.*[A-Za-z]{2})(?=.*\d{2}).{8,}$/,
+      "Password must be at least 8 characters long, with at least 2 letters and 2 digits"
+    )
+    .required("Password is required"),
+});
+//?       Validation changed!
