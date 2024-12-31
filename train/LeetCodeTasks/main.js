a8cf78cff0b3a12b149c8a5ec4efbde3dde47baa
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-12-31 22:46:00 +0100

    132

--- train/LeetCodeTasks/main.js
+++ train/LeetCodeTasks/main.js
@@ -1,25 +1,27 @@
-const nums = [1, 0, 1, 1, 1];
+//? ================================== Task 1 =======================================
+const nums = [1, 1, 0, 1, 1, 1, 0, 1, 1];
 
-function findMaxConsecutiveOnes(nums) {
-  let howMuchNumber = 0;
-  const numbers = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === 1) {
-      numbers.push(nums[i]);
-    } else {
-      if (!howMuchNumber <= numbers.length) {
-        howMuchNumber = numbers.length;
-        numbers.splice(0, numbers.length);
-      } else {
-      }
-    }
-  }
-  return howMuchNumber;
-}
+console.log(nums.length);
 
-// findMaxConsecutiveOnes(nums);
-console.log(findMaxConsecutiveOnes(nums));
+function find 
 
-// for (let i = 0; i < nums.length; i++) {
-//   console.log(nums[i]);
+// function findMaxConsecutiveOnes(nums) {
+//   let howMuchNumber = 0;
+//   const numbers = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === 0) {
+//       if (howMuchNumber <= numbers.length) {
+//         howMuchNumber = numbers.length;
+//         console.log(howMuchNumber);
+//         numbers.splice(0, numbers.length);
+//       }
+//     } else {
+//       numbers.push(nums[i]);
+//       if (nums.length === i + 1 && numbers.length > howMuchNumber) {
+//         howMuchNumber = numbers.length;
+//       }
+//     }
+//   }
+//   return howMuchNumber;
 // }
+// console.log(findMaxConsecutiveOnes(nums));
