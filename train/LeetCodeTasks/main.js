34e6af1d436d36987fb90c7ea54aa416a45c0828
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2025-01-02 11:54:21 +0100

    123

--- train/LeetCodeTasks/main.js
+++ train/LeetCodeTasks/main.js
@@ -1,10 +1,23 @@
-//? ================================== Task 1 =======================================
-const nums = [1, 1, 0, 1, 1, 1, 0, 1, 1];
+//? ============================================== Task 1 ===========================================================
+// const nums = [1, 1, 0, 1, 1, 1, 1, 1];
 
-console.log(nums.length);
+// function findMaxConsecutiveOnes(nums) {
+//   let maxCount = 0; // Максимальна кількість послідовних одиниць
+//   let currentCount = 0; // Поточна кількість послідовних одиниць
 
-function find 
+//   for (let num of nums) {
+//     if (num === 1) {
+//       currentCount++; // Збільшуємо лічильник, якщо знаходимо 1
+//       maxCount = Math.max(maxCount, currentCount); // Оновлюємо максимальне значення
+//     } else {
+//       currentCount = 0; // Скидаємо лічильник, якщо знаходимо 0
+//     }
+//   }
 
+//   return maxCount; // Повертаємо максимальну кількість послідовних одиниць
+// }
+
+//?My resolve... Not good. And wrong...
 // function findMaxConsecutiveOnes(nums) {
 //   let howMuchNumber = 0;
 //   const numbers = [];
@@ -24,4 +37,37 @@ function find
 //   }
 //   return howMuchNumber;
 // }
+
 // console.log(findMaxConsecutiveOnes(nums));
+
+//? =============================================== Task 2 ==========================================================
+// const nums = [12, 345, 2, 6, 7896];
+// const nums = [555, 901, 482, 1771];
+
+// function findNumbers(nums) {
+//   let count = 0; // Лічильник чисел з парною кількістю цифр
+
+//   for (let i = 0; i < nums.length; i++) {
+//     const numLength = nums[i].toString().length; // Перетворюємо число на рядок і отримуємо кількість цифр
+//     if (numLength % 2 === 0) {
+//       // Перевіряємо, чи кількість цифр є парною
+//       count++;
+//     }
+//   }
+//   return count; // Повертаємо кількість чисел з парною кількістю цифр
+// }
+
+//? My resolve. Not work but how ?...
+// function findNumbers(nums) {
+//   let result = 0;
+//   for (let i = 0; nums.length > i; i++) {
+//     if (nums[i] % 2 === 0 && nums[i].toString().length > 1) {
+//       result++;
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(findNumbers(nums));
+
+//?======================================================= Task 3 ===============================================================
