ab85f66ddde19e26f623d519f3852a312a08b643
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2025-01-04 15:36:52 +0100

    1

--- train/LeetCodeTasks/main.js
+++ train/LeetCodeTasks/main.js
@@ -113,3 +113,59 @@
 
 // console.log(duplicateZeros(arr));
 //?============================================== Task 5 ======================================================================
+// const nums1 = [1, 2, 3, 0, 0, 0];
+// const m = 3;
+// const nums2 = [2, 5, 6];
+// const n = 3;
+
+//?First resolve... Is wrong. Needed return nums1!
+// function merge(nums1, m, nums2, n) {
+//   const result = [...nums1.slice(0, m), ...nums2.slice(0, n)];
+//   return result.sort((a, b) => a - b);
+// }
+
+//?Second resolve changed and sorted array nums1.
+// function merge(nums1, m, nums2, n) {
+//   nums1.splice(m);
+//   nums1.push(...nums2.slice(0, n));
+//   return nums1.sort((a, b) => a - b);
+// }
+
+// console.log(merge(nums1, m, nums2, n));
+
+//?====================================================== Task 6 =======================================================
+// const nums = [3, 2, 2, 3];
+// const val = 3;
+
+// const nums = [0, 1, 2, 2, 3, 0, 4, 2];
+// const val = 2;
+
+//?My answer. Wrong again!!! WTF?
+// function removeElement(nums, val) {
+//   for (let i = 0; nums.length > i; i++) {
+//     if (nums[i] === val) {
+//       nums.splice(i, 1);
+//       i--;
+//       nums.push("_");
+//     }
+//   }
+//   return nums;
+// }
+
+//?Not my answer!
+// function removeElement(nums, val) {
+//   let k = 0; // Лічильник для кількості елементів, які не рівні `val`
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== val) {
+//       nums[k] = nums[i]; // Переміщаємо значення, яке не рівне `val`, у початок масиву
+//       k++;
+//     }
+//   }
+//   console.log(nums);
+//   return k; // Повертаємо кількість елементів, які не рівні `val`
+// }
+// console.log(removeElement(nums, val));
+
+//?========================================================== Task 7 ===================================================
+function removeDuplicates(nums) {}
